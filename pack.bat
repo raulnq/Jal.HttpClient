961b8d833880132a899fef7fd80576e4bba258be
Raul Naupari <raul.naupari@cignium.com>
2015-12-21 11:27:28 -0500

    Initial commit

---
+++ pack.bat
@@ -0,0 +1,20 @@
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop\Jal.Aop.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects\Jal.Aop.Aspects.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Advice\Jal.Aop.Aspects.Advice.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Advice.Installer\Jal.Aop.Aspects.Advice.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Installer\Jal.Aop.Aspects.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Logger\Jal.Aop.Aspects.Logger.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Serializer\Jal.Aop.Aspects.Serializer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Serializer.Installer\Jal.Aop.Aspects.Serializer.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Aspects.Logger.Installer\Jal.Aop.Aspects.Logger.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.Aop.Installer\Jal.Aop.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Aop.Nuget
+pause;
